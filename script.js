c256948d2bac3482459d8ca689a55d2b56b45ce3
olegborovych <olborovych@gmail.com>
2020-07-02 18:44:27 +0300

    dom1

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1.Создать страницу, которая выводит нумерованный список песен:
+
+let playList = [
+    {
+        author: "LED ZEPPELIN",
+        song: "STAIRWAY TO HEAVEN"
+    },
+    {
+        author: "QUEEN",
+        song: "BOHEMIAN RHAPSODY"
+    },
+    {
+        author: "LYNYRD SKYNYRD",
+        song: "FREE BIRD"
+    },
+    {
+        author: "DEEP PURPLE",
+        song: "SMOKE ON THE WATER"
+    },
+    {
+        author: "JIMI HENDRIX",
+        song: "ALL ALONG THE WATCHTOWER"
+    },
+    {
+        author: "AC/DC",
+        song: "BACK IN BLACK"
+    },
+    {
+        author: "QUEEN",
+        song: "WE WILL ROCK YOU"
+    },
+    {
+        author: "METALLICA",
+        song: "ENTER SANDMAN"
+    }
+];
+
+for (let i = 1; i <= playList.length; i++)
+    document.write([i])

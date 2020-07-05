38a2721657faf672837267b05533c53168a78e6a
olegborovych <olborovych@gmail.com>
2020-07-05 03:26:17 +0300

    h27_v3

--- script.js
+++ script.js
@@ -30,27 +30,59 @@
 
 // 3 . Функция вывода времени на экран;
 
-let time = {
-    hour: 2,
-    minute: 13,
-    second: 33,
-};
+// let time = {
+//     hour: 12,
+//     minute: 43,
+//     second: 54,
+// };
 
-function showDate(obj) {
-    for (let i in obj) {
-        document.write(`${i}:<h1>${obj[i]}</h1>`);
-    }
-}
-showDate(time);
+// function showDate(obj) {
+//     for (let i in obj) {
+//         document.write(`${i}:<h1>${obj[i]}</h1>`);
+//     }
+// }
+// showDate(time);
 
-// todo Функция изменения времени на переданное количество секунд;
+// let time = {
+//     hour: 13,
+//     minute: 49,
+//     seconds: 24,
+// }
+
+// function showTime() {
+//     document.write(`<h1>${time.hour}:${time.minute}:${time.seconds}</h1>`);
+// }
+// showTime()
+
+// // todo Функция изменения времени на переданное количество секунд;
+
+// let setSeconds = prompt('Введіть к-сть секунд');
+// if 
+
+// function addSeconds() {
+//         let newSeconds = +time.seconds + +setSeconds;
+//         document.write(`<h1>${time.hour}:${time.minute}:${newSeconds}</h1>`);
+//     }
+
+// addSeconds()
 
 
+// // todo Функция изменения времени на переданное количество минут;
 
-// todo Функция изменения времени на переданное количество минут;
+// let setMinute = prompt('Веддіть к-сть хвилин');
 
+// function addMinute() {
+//     let newMinute = +time.minute + +setMinute;
+//     document.write(`<h1>${time.hour}:${newMinute}:${time.seconds}</h1>`);
+// }
+// addMinute()
 
+// //todo Функция изменения времени на переданное количество часов.
+// let setHour = prompt('Веддіть к-сть годин');
 
-//todo Функция изменения времени на переданное количество часов. 
-
+// function addHours() {
+//     let newHours = +time.hour + +setHour;
+//     document.write(`<h1>${newHours}:${time.minute}:${time.seconds}</h1>`);
+// }
+// addHours()
 
